77c540f07b0bbaaed5bad0312049842509159d2f
SrGodoy <luccasgodoy2012@gmail.com>
2024-11-20 12:55:17 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'p2_mercado',
+  webDir: 'www'
+};
+
+export default config;
